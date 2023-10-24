15cece71cb178fd81850ccf520034be32430d863
Lindsey <lindsey@Lindseys-MacBook-Pro.local>
2023-10-24 19:39:07 -0400

    icons and contact

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,6 +1,25 @@
+import linkedIn from '../assets/icons/linkedinIcon.png'
+import gitHub from '../assets/icons/githubIcon.png'
+import email from '../assets/icons/emailIcon.png'
+
+
 export default function Contact(){
     return(
-        <h1>Contact hellloooooo</h1>
+        <>
+        <a className='icon' href='https://github.com/LindseyHsiao'>
+            <img src={email} alt="linkedIn" className='icon' />
+        </a>
+
+        <a href='https://github.com/LindseyHsiao'>
+            <img src={linkedIn} alt="linkedIn" className='color: white' />
+        </a>
+
+        <a href='https://github.com/LindseyHsiao'>
+            <img src={gitHub} alt="linkedIn" className='color: white' />
+        </a>
+
+        
+</>
 
     )
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import portfolioPic from '../assets/images/portfolioPic.png'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
     return (
@@ -6,11 +7,15 @@ export default function Home() {
             <div className="home-div-left">
                 <h1 className='home-title'>Full Stack Web Developer</h1>
                 <h2 className="home-text">
-                    With experience in React, Mongo DB, Express and Node.js as a full stack develper, I look forward to  
+                    With experience in React, Mongo DB, Express and Node.js as a full stack develper, I look forward to seeing what we can build together. 
                     
                 </h2>
-                
+                <Link to="/lindsey-hsiao-portfolio/Contact">
+                {/* <a href='mailto:lindsey350@aol.com'> */}
+
                 <button className='home-button'>Lets Connect!</button>
+                {/* </a> */}
+                </Link>
             </div>
             <div className="home-div-right">
                 <img src={portfolioPic} alt="Lindsey" />
