805211bd8bc9e03051cfc95faf8d22113e84c0eb
Lindsey <lindsey@Lindseys-MacBook-Pro.local>
2023-10-24 11:31:26 -0400

    css'

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 export default function Navigation() {
     return (
         <nav className="nav">
-            <Link to="/" className="site-title">Lindsey Hsiao</Link>
+            <Link to="/lindsey-hsiao-portfolio" className="site-title">Lindsey Hsiao</Link>
             <ul>
                 <li>
                     <Link to="/Projects">Projects</Link>
