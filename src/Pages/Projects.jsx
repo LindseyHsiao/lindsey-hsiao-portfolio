8c1d76f2afed244e4649a054508b7ffa5723b49e
Lindsey <lindsey@Lindseys-MacBook-Pro.local>
2023-10-26 10:39:22 -0400

    favicon

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,18 +1,32 @@
+import runBuddy from '../assets/images/runbuddy.png'
+import onespark from '../assets/images/onespark.png'
 
-// const projects = [
-//     {
-//         title: title,
-//         img: img,
-//         description: description,
-//         link: link
-//     },
-//     {
-//         title: title,
-//         img: img,
-//         description: description,
-//         link: link
-//     },
-// ]
+const projects = [
+    {
+        title: 'Run Buddy',
+        img: runBuddy,
+        description: "description",
+        link: 'link'
+    },
+    {
+        title: 'One Spark',
+        img: onespark,
+        description: 'description',
+        link: 'link'
+    },
+    {
+        title: 'One Spark',
+        img: onespark,
+        description: 'description',
+        link: 'link'
+    },
+    {
+        title: 'One Spark',
+        img: onespark,
+        description: 'description',
+        link: 'link'
+    },
+]
 
 export default function Projects() {
     return (
@@ -20,14 +34,19 @@ export default function Projects() {
             <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flex: '0 0 60%' }}>
                 <h1>Portfolio</h1>
             </div>
-            {/* {projects.map((item, i) => (
-
-            ))} */}
             <div style={{flex: '0 0 38%', display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center', padding: '2% 0'}}>
+            {projects.map((item, i) => (
+                <a href="#" style={{border: 'black solid 2px', minHeight: '150px', maxHeight: '150px', backgroundColor: 'red', margin: '2px', flexBasis: '45%'}} key={i}>
+                    <img src={item.img} alt="" width={100}/>
+                    <div>
+                        <h3>{item.title}</h3>
+                    </div>
+
+                </a>
+            ))}
+                {/* <a href="#" style={{border: 'black solid 2px', minHeight: '150px', maxHeight: '150px', backgroundColor: 'red', margin: '2px', flexBasis: '45%'}}>test</a>
                 <a href="#" style={{border: 'black solid 2px', minHeight: '150px', maxHeight: '150px', backgroundColor: 'red', margin: '2px', flexBasis: '45%'}}>test</a>
-                <a href="#" style={{border: 'black solid 2px', minHeight: '150px', maxHeight: '150px', backgroundColor: 'red', margin: '2px', flexBasis: '45%'}}>test</a>
-                <a href="#" style={{border: 'black solid 2px', minHeight: '150px', maxHeight: '150px', backgroundColor: 'red', margin: '2px', flexBasis: '45%'}}>test</a>
-                <a href="#" style={{border: 'black solid 2px', minHeight: '150px', maxHeight: '150px', backgroundColor: 'red', margin: '2px', flexBasis: '45%'}}>test</a>
+                <a href="#" style={{border: 'black solid 2px', minHeight: '150px', maxHeight: '150px', backgroundColor: 'red', margin: '2px', flexBasis: '45%'}}>test</a> */}
             </div>
         </section>
 
