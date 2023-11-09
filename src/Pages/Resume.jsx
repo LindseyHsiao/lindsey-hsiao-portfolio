a13639138ac18546280ac18aafd096004270aa3c
Lindsey <lindsey@Lindseys-MacBook-Pro.local>
2023-11-08 21:00:04 -0500

    updated resume link

--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -5,7 +5,7 @@ export default function Resume() {
 
 
     return (
-        <a href='https://drive.google.com/file/d/1Bx2hpTW3X_aRr6SnfscxPw-pNNpwagyR/view?usp=sharing'>
+        <a href='https://drive.google.com/file/d/1Gy-K6BIoJwnCIsMTukteKncSGs-WJaoM/view?usp=sharing'>
             <h1 className='resume-text'>Click here to view resume PDF</h1>
         </a>
 
